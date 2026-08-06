58599640af000646ddccb2d255d84a9c4d315ceb
Thomas Sam <wookat@qq.com>
2026-08-06 09:51:58 +0000

    Round 91: add Generator to primary nav

--- src/html.js
+++ src/html.js
@@ -59,6 +59,7 @@ ${jsonld ? `<script type="application/ld+json">${JSON.stringify(jsonld)}</script
       <a class="hover:text-indigo-700" href="/top/girls">Girls</a>
       <a class="hover:text-indigo-700" href="/top/boys">Boys</a>
       <a class="hover:text-indigo-700" href="/trending">Trending</a>
+      <a class="hover:text-indigo-700 hidden sm:inline" href="/generator">Generator</a>
       <a class="hover:text-indigo-700" href="/browse">Browse</a>
     </nav>
   </div>
